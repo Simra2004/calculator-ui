bbf668e9a8ddb4d46c54ddf80f2087cf7ee63755
Simra Faisal <115410594+Simra2004@users.noreply.github.com>
2025-02-10 15:12:59 +0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,19 @@
+const result = document.getElementById("result");
+document.querySelectorAll(".btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (button.classList.contains("clear")) {
+      result.value = "";
+    } else if (button.classList.contains("delete")) {
+      result.value = result.value.slice(0, -1);
+    } else if (button.classList.contains("equals")) {
+      try {
+        result.value = eval(result.value) || "";
+      } catch (e) {
+        result.value = "Error";
+      }
+    } else {
+      result.value = value++;
+    }
+  });
+});
